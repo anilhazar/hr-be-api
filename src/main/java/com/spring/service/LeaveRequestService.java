3933778a6d1dd7f91ad0ca61c1f34c30933fe075
Ali Anıl Hazar <112640395+anilhazar@users.noreply.github.com>
2024-02-24 13:17:15 +0300

    Revert "PR : Issue #7 : Create LeaveRequest Endpoint"

--- src/main/java/com/spring/service/LeaveRequestService.java
+++
@@ -1,8 +0,0 @@
-package com.spring.service;
-
-import com.spring.model.dto.request.LeaveRequestCreateRequest;
-import com.spring.model.entity.LeaveRequestEntity;
-
-public interface LeaveRequestService {
-    LeaveRequestEntity createLeaveRequest(LeaveRequestCreateRequest leaveRequestCreateRequest);
-}
