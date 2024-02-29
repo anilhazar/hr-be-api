7c7d392f152860b1d48f905a8f332a79c71360df
anilhazar <alianilhazar@gmail.com>
2024-02-29 21:59:34 +0300

    Faz1

--- src/main/java/com/spring/repository/impl/EmployeeRepositoryConstants.java
+++ src/main/java/com/spring/repository/impl/EmployeeRepositoryConstants.java
@@ -1,7 +1,5 @@
 package com.spring.repository.impl;
 
-import org.sql2o.Connection;
-
 public class EmployeeRepositoryConstants {
     static final String FIND_ALL = "SELECT id, name, surname, age, gender, username, password FROM employee";
 
