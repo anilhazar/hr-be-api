66037e71de9ac2275f8ad99afc1d3fee0ab0d3c9
anilhazar <alianilhazar@gmail.com>
2024-03-14 16:49:08 +0300

    Insert Mailing System

---
+++ src/main/java/com/spring/service/EmailService.java
@@ -0,0 +1,6 @@
+package com.spring.service;
+
+public interface EmailService {
+
+    void send(String email, String subject, String body);
+}
---
+++ src/main/java/com/spring/service/impl/EmployeeEmailServiceImpl.java
@@ -0,0 +1,30 @@
+package com.spring.service.impl;
+
+import com.spring.model.entity.EmployeeEntity;
+import com.spring.service.EmailService;
+import com.spring.service.EmployeeEmailService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeEmailServiceImpl implements EmployeeEmailService {
+
+    private final EmailService emailService;
+
+    public EmployeeEmailServiceImpl(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @Override
+    public void sendUsernameAndPassword(EmployeeEntity employeeEntity) {
+
+        String subject = "Your Account Information";
+        String content = "Dear " + employeeEntity.getFirstName() + ",\n\n"
+                + "We are pleased to inform you that your account has been created successfully. Please find your login credentials below:\n\n"
+                + "Username: " + employeeEntity.getUsername() + "\n"
+                + "Temporary Password: " + employeeEntity.getPassword() + "\n\n"
+                + "Best regards,\n";
+
+        emailService.send(employeeEntity.getEmail(), subject, content);
+
+    }
+}
