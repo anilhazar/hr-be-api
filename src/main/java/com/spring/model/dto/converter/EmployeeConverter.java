f61aaf320de6e510b0b0bf6306e8d61c1e40e314
anilhazar <alianilhazar@gmail.com>
2024-05-09 15:06:57 +0300

    Authentication Service + Converter Classes

---
+++ src/main/java/com/spring/model/dto/converter/EmployeeConverter.java
@@ -0,0 +1,39 @@
+package com.spring.model.dto.converter;
+
+import com.spring.model.dto.request.employee.EmployeeCreateRequest;
+import com.spring.model.dto.response.EmployeeResponse;
+import com.spring.model.entity.EmployeeEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeConverter {
+    public static EmployeeEntity toEntity(EmployeeCreateRequest employeeCreateRequest) {
+        return new EmployeeEntity(
+                employeeCreateRequest.getFirstName(),
+                employeeCreateRequest.getLastName(),
+                employeeCreateRequest.getGender(),
+                employeeCreateRequest.getAge(),
+                employeeCreateRequest.getEmail());
+    }
+
+    public static EmployeeResponse toResponse(EmployeeEntity employeeEntity) {
+        return new EmployeeResponse(
+                employeeEntity.getFirstName(),
+                employeeEntity.getLastName(),
+                employeeEntity.getAge(),
+                employeeEntity.getGender(),
+                employeeEntity.getEmail()
+        );
+    }
+
+    public static List<EmployeeResponse> toResponse(List<EmployeeEntity> employeeEntities) {
+        List<EmployeeResponse> employeeResponses = new ArrayList<>();
+        for (EmployeeEntity employeeEntity : employeeEntities) {
+            EmployeeResponse employeeResponse = toResponse(employeeEntity);
+            employeeResponses.add(employeeResponse);
+        }
+        return employeeResponses;
+
+    }
+}
