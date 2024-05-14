6fe0e9e6a6930045e1b7ce942221db2e30d64c77
anilhazar <alianilhazar@gmail.com>
2024-05-14 23:58:47 +0300

    Implement daily birthday email notifications:
    Development of the functionality to send birthday emails to users every day at 9 AM completed successfully

--- src/main/java/com/spring/service/impl/EmployeeEmailServiceImpl.java
+++ src/main/java/com/spring/service/impl/EmployeeEmailServiceImpl.java
@@ -1,24 +1,32 @@
 package com.spring.service.impl;
 
 import com.spring.model.entity.EmployeeEntity;
+import com.spring.repository.EmployeeRepository;
 import com.spring.service.EmailService;
 import com.spring.service.EmployeeEmailService;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 class EmployeeEmailServiceImpl implements EmployeeEmailService {
 
     private final EmailService emailService;
+    private final EmployeeRepository employeeRepository;
 
-    public EmployeeEmailServiceImpl(EmailService emailService) {
+    public EmployeeEmailServiceImpl(EmailService emailService, EmployeeRepository employeeRepository) {
         this.emailService = emailService;
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
     public void sendUsernameAndPassword(EmployeeEntity employeeEntity) {
 
         String subject = "Your Account Information";
-        String content = "Dear " + employeeEntity.getFirstName() + ",\n\n"
+        String content = "Dear " + employeeEntity.getfirstName() + ",\n\n"
                 + "We are pleased to inform you that your account has been created successfully. Please find your login credentials below:\n\n"
                 + "Username: " + employeeEntity.getUsername() + "\n"
                 + "Temporary Password: " + employeeEntity.getPassword() + "\n\n"
@@ -27,4 +35,48 @@ class EmployeeEmailServiceImpl implements EmployeeEmailService {
         emailService.send(employeeEntity.getEmail(), subject, content);
 
     }
+
+    /**
+     * Asynchronously sends birthday emails to employees whose birthday is today.
+     * The method retrieves a list of employees, compares their birthdays with today's date,
+     * and sends personalized birthday emails to the relevant employees.
+     * The email content includes a birthday greeting and best wishes.
+     *
+     * @throws RuntimeException if no employees are found in the repository.
+     * @implNote The method uses the @Async annotation to execute the task asynchronously,
+     * allowing other operations to continue without blocking the main thread.
+     */
+    @Async
+    @Scheduled(cron = "0  9  * * *")
+    public void sendBirthdayEmail() {
+
+        List<EmployeeEntity> employees = employeeRepository.findAllEmployee()
+                .orElseThrow(() -> new RuntimeException("No Employee Found"));
+        LocalDate today = LocalDate.now();
+        LocalDate userBirthday;
+        boolean isSameDayAndMonth;
+
+        for (EmployeeEntity employee : employees) {
+            userBirthday = employee.getBirthday();
+            isSameDayAndMonth = today.getMonth() == userBirthday.getMonth() &&
+                    today.getDayOfMonth() == userBirthday.getDayOfMonth();
+            if (!isSameDayAndMonth) {
+                employees.remove(employee);
+            }
+        }
+
+        employees.forEach(employeeEntity -> {
+            String subject = "\uD83C\uDF82 Happy Birthday! \uD83C\uDF89";
+            String content = "Dear " + employeeEntity.getFirstName() + ",\n"
+                    + "On this special day, may your heart be filled with joy, your path be lined with love, "
+                    + "and your dreams soar higher than ever. May each moment be a celebration, "
+                    + "and may the year ahead bring you countless reasons to smile. \\n\""
+                    + "Wishing you a day as bright as your spirit and a year as wonderful as your presence. "
+                    + "Cheers to another amazing trip around the sun! \uD83C\uDF1F"
+                    + "Best wishes from all of us at Castilla Of Developers A.Ş!\" \uD83C\uDF88\uD83C\uDF81";
+
+            emailService.send(employeeEntity.getEmail(), subject, content);
+        });
+    }
+
 }
