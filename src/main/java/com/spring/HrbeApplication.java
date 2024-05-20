9998377a0e209ab62926da63208f8e1ec9690f8b
anilhazar <alianilhazar@gmail.com>
2024-05-20 12:39:46 +0300

    refactor sql script

--- src/main/java/com/spring/HrbeApplication.java
+++ src/main/java/com/spring/HrbeApplication.java
@@ -8,8 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class HrbeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HrbeApplication.class, args);
-    }
+    public static void main(String[] args) {SpringApplication.run(HrbeApplication.class, args);}
 
 }
--- src/main/resources/sql/hr-be-api.sql
+++ src/main/resources/sql/hr-be-api.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE hr_be_api;
+CREATE DATABASE IF NOT EXISTS hr_be_api;
 
 USE hr_be_api;
 
-CREATE TABLE employee
+CREATE TABLE IF NOT EXISTS employee
 (
     id         BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     first_name VARCHAR(24)                       NOT NULL,
@@ -11,26 +11,24 @@ CREATE TABLE employee
     birthday   DATE                              NOT NULL,
     age        INT                               NOT NULL,
     email      VARCHAR(48)                       NOT NULL,
-    username   VARCHAR(24)  NOT NULL,
-    password   VARCHAR(240) NOT NULL
-);
+    username   VARCHAR(24) DEFAULT NULL,
+    password   VARCHAR(240) DEFAULT NULL
+    );
 
-CREATE TABLE leave_request
+CREATE TABLE IF NOT EXISTS leave_request
 (
     id           BIGINT AUTO_INCREMENT PRIMARY KEY               NOT NULL,
     employee_id  BIGINT                                          NOT NULL,
     request_type ENUM ('BIRTH', 'ANNUAL', 'PATERNITY', 'EXCUSE') NOT NULL,
-    status ENUM ('APPROVED', 'REJECTED', 'PENDING')              NOT NULL,
-    create_date  DATE                                            NOT NULL,
+    status       ENUM ('APPROVED', 'REJECTED', 'PENDING')        NOT NULL,
+    create_date  DATE                                            DEFAULT NULL,
     start_date   DATE                                            NOT NULL,
     end_date     DATE                                            NOT NULL,
     CONSTRAINT fk__employee_id__leave_request FOREIGN KEY (employee_id) REFERENCES employee (id)
-);
+    );
 
 INSERT INTO employee (first_name, last_name, gender, birthday, age, email)
-VALUES ('John', 'Doe', 'Male', 30, '1996-08-17', 'johndoe@example.com');
+VALUES ('John', 'Doe', 'Male', '1996-08-17', 30, 'johndoe@example.com');
 
-INSERT INTO leave_request (employee_id, request_type, status, start_date, end_date)
-VALUES (1, 'ANNUAL', 'PENDING', '2024-06-01', '2024-06-10');
-
-
+INSERT INTO leave_request (employee_id, request_type, status, create_date, start_date, end_date)
+VALUES (1, 'ANNUAL', 'PENDING', CURDATE(), '2024-06-01', '2024-06-10');
